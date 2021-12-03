3f6d4027354141d64fadca75b2a2cc10bd55f6a0
Cuelox <a.hayat9000@gmail.com>
2021-12-03 17:03:09 +0500

    changes

--- container/SinglePage/AccommodationPolicies/AccommodationPolicies.js
+++ container/SinglePage/AccommodationPolicies/AccommodationPolicies.js
@@ -2,53 +2,48 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Heading from 'components/UI/Heading/Heading';
 import Text from 'components/UI/Text/Text';
-import AccommodationPoliciesArea, { APWrapper } from './AccommodationPolicies.style';
-import Map from 'components/Map/Map';
+import AccommodationPoliciesArea, { AccommodationPolicieswrapper } from './AccommodationPolicies.style';
 import { Element } from 'react-scroll';
 
 
 const AccommodationPolicies = ({
   titleStyle,
   titleStyleAP,
-  locationMetaStyle,
-  contentStyle,
-  boldContentStyle,
-  linkStyle,
-  location,
+  detailstyle,
 }) => {
   return (
     <Element name="AccommodationPolicies" className="AccommodationPolicies">
+      
       <AccommodationPoliciesArea>
         <Heading as="h2" content="Accommodation Policies" {...titleStyleAP} />
-          <Heading as="h2" content="Rent & Bills"{...titleStyle}/>
-          <Text
-            content="Rent and utilities are due every 30 days.."
-            {...contentStyle}
-          />
-        <Heading as="h2" content="Security Deposit"{...titleStyle}/>
-        <Text
-          content="A €1600 security deposit is paid directly to your accommodation provider. In the event you move out earlier than expected or there are damages during your stay, the accommodation provider may keep a portion or all of the deposit."
-          {...contentStyle}
-        />
-
-        <Heading as="h2" content="Rules"{...titleStyle}/>
-        <Text
-          content="Respect of the apartment and the neighbors. No noises or parties between 8pm - 8am"
-          {...contentStyle}
-        />
+        <AccommodationPolicieswrapper>
+          <Heading as="span" content="Rent & Bills"{...titleStyle}/>
+          <Text as="span" style={{ margin: '20px !important' }} content="Rent and utilities are due every 30 days.."
+            {...detailstyle}/>
+        </AccommodationPolicieswrapper>
         
-        <Heading as="h2" content="Cancellation Policy"{...titleStyle}/>
-        <Text
-          content="Moderate:  If a user cancels up to 60 days before move in date, there is a 50% refund for the payment on the first month rent payment. If the user cancels up to 30 days before move in date there is a 25% refund for the payment on the first month rent payment. If the user cancels under 30 days before move in date, there is no refund available for the payment on the first month rent payment."
-          {...contentStyle}
-        />
+        <AccommodationPolicieswrapper>
+          <Heading as="span" content="Security Deposit"{...titleStyle}/>
+          <Text as="span" style={{ margin: '20px !important' }} content="A €1600 security deposit is paid directly to your accommodation provider. In the event you move out earlier than expected or there are damages during your stay, the accommodation provider may keep a portion or all of the deposit."
+            {...detailstyle}/>
+        </AccommodationPolicieswrapper>
         
-        <Heading as="h2" content="Contract Type"{...titleStyle}/>
-        <Text
-          content="Pro-rated: Rent is calculated in proportion to the number of days of your stay. You will pay the monthly rent for each whole month that you book, but the first and last month you will simply pay for the days that you stay (like a hotel)"
-          {...contentStyle}
-        />
-
+        <AccommodationPolicieswrapper>
+          <Heading as="span" content="Rules"{...titleStyle}/>
+          <Text as="span" style={{ margin: '20px !important' }} content="Respect of the apartment and the neighbors. No noises or parties between 8pm - 8am"
+           {...detailstyle}/>
+        </AccommodationPolicieswrapper>
+        
+        <AccommodationPolicieswrapper>
+          <Heading as="span" content="Cancellation Policy"{...titleStyle}/>
+          <Text as="span" style={{ margin: '20px !important' }} content="Moderate:  If a user cancels up to 60 days before move in date, there is a 50% refund for the payment on the first month rent payment. If the user cancels up to 30 days before move in date there is a 25% refund for the payment on the first month rent payment. If the user cancels under 30 days before move in date, there is no refund available for the payment on the first month rent payment."
+            {...detailstyle}/>
+        </AccommodationPolicieswrapper>
+        <AccommodationPolicieswrapper>
+          <Heading as="span" content="Contract Type"{...titleStyle}/>
+          <Text as="span" style={{ margin: '20px !important' }} content="Pro-rated: Rent is calculated in proportion to the number of days of your stay. You will pay the monthly rent for each whole month that you book, but the first and last month you will simply pay for the days that you stay (like a hotel)"
+            {...detailstyle}/>
+        </AccommodationPolicieswrapper>
       </AccommodationPoliciesArea>
     </Element>
   );
@@ -57,29 +52,36 @@ const AccommodationPolicies = ({
 AccommodationPolicies.propTypes = {
   titleStyle: PropTypes.object,
   titleStyleAP: PropTypes.object,
-  locationMetaStyle: PropTypes.object,
-  contentStyle: PropTypes.object,
+  detailstyle: PropTypes.object,
 };
 
 AccommodationPolicies.defaultProps = {
+
   titleStyleAP: {
     color: '#2C2C2C',
+    marginBottom: '12px',
     fontSize: ['17px', '20px', '25px'],
     lineHeight: ['1.15', '1.2', '1.36'],
     mb: '4px',
   },
+      
+  detailstyle: {
+    fontSize: '16px',
+    textAlign: 'right',
+    color: '#232323',
+    position: 'absolute', 
+    borderLeft: '#888 solid 1px',
+    paddingLeft: '20px',
+  },
+
   titleStyle: {
-    color: '#2C2C2C',
-    fontSize: ['17px', '20px', '20px'],
-    lineHeight: ['1.15', '1.2', '2.36'],
-    mb: '4px',
+    fontSize: '20px',
+    color: '#767676',
+    fontWeight: '500',
+    paddingLeft: '0px', 
+    top: '0'
   },
-  locationMetaStyle: {
-    fontSize: '13px',
-    fontWeight: '400',
-    color: '#909090',
-    mb: ['14px', '20px', '27px'],
-  },
+ 
   contentStyle: {
     fontSize: '15px',
     fontWeight: '400',
@@ -87,15 +89,7 @@ AccommodationPolicies.defaultProps = {
     lineHeight: '1.6',
     mb: ['14px', '20px', '27px'],
   },
-  boldContentStyle: {
-    fontWeight: '700',
-    mb: '0!important',
-  },
-  linkStyle: {
-    fontSize: '15px',
-    fontWeight: '700',
-    color: '#0088E5',
-  },
+
 };
 
 export default AccommodationPolicies;
--- container/SinglePage/AccommodationPolicies/AccommodationPolicies.style.js
+++ container/SinglePage/AccommodationPolicies/AccommodationPolicies.style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { themeGet } from '@styled-system/theme-get';
 
 const AccommodationPoliciesArea = styled.div`
   padding: 29px 0;
@@ -10,6 +11,20 @@ const AccommodationPoliciesArea = styled.div`
       color: #31b8bd;
     }
   }
+`;
+
+export const ContentArea1 = styled.div`
+  padding: 29px 0;
+  float: left;
+`;
+export const ContentArea2 = styled.div`
+  padding: 29px 0;
+  float: right;
+`;
+export const AccommodationPolicieswrapper = styled.div`
+  border-radius: 4px;
+  padding: 0px 0px 30px;
+  margin-bottom: 5px;
 `;
 
 export default AccommodationPoliciesArea;
