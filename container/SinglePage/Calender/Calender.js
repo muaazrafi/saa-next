e5a184220f808dff2520cb62b1cd59fb2f71e360
Muaaz Rafi <tomuaaz@gmail.com>
2021-12-22 22:35:53 +0500

    Disabled the dates on the calender

--- container/SinglePage/Calender/Calender.js
+++ container/SinglePage/Calender/Calender.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import Link from 'next/link';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import Heading from 'components/UI/Heading/Heading';
-import {Row,Col,Divider,Calendar} from 'antd';
+import {Row,Col, Calendar} from 'antd';
 import { FaSquare } from 'react-icons/fa';
 import CalenderWrapper from './Calender.style';
 import { Element } from 'react-scroll';
 
 const Calender = ({ calenderStyle }) => {
+  const { apartment } = useSelector( (state) => state.apartment);
+
+  useEffect( () => {
+  }, [apartment]);
+
+  const disabledDate = (current) => {
+    if (current && current.valueOf() < Date.now()) {
+      return true;
+    }
+    if ( apartment && apartment.occupied_dates ) {
+      return apartment.occupied_dates.includes(current.format("YYYY/MM/DD"));
+    }
+    // Implement check in windows logic here
+  }
+
+
   return (
     <Element name="calendar">
       <CalenderWrapper>
         <Heading as="h2" content="Availability"  />
-      	<Calendar fullscreen={false} {...calenderStyle}/>
+      	<Calendar disabledDate={disabledDate} fullscreen={false} {...calenderStyle} />
 	  	    <Row >
 	        	<Col span={24}>{<FaSquare style= {{color: '#d9534f'}} />} Red dates above are confirmed as occupied.</Col>
 	        </Row>	
