d67feeb8b1dd588f2b9b47deb20edefce3a7560a
Muaaz Rafi <tomuaaz@gmail.com>
2022-02-04 16:27:00 +0500

    Enabling deployment on staging server

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: "SAA",
+    script: "yarn",
+    args: "prod",
+    instances  : 1,
+    exec_mode  : "cluster"
+  }],
+  deploy : {
+    staging : {
+      user : 'ubuntu',
+      host : '18.142.53.216',
+      ref  : 'origin/dev',
+      repo : 'https://muaazrafi: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@@github.com/muaazrafi/saa-next.git',
+      path : '/home/ubuntu/saa_next',
+      'pre-deploy-local': '',
+      'post-deploy' : 'nvm use 16.13.0 && yarn && yarn build && pm2 restart ecosystem.config.js',
+      'pre-setup': ''
+    },
+    // production : {
+    //   user : 'ubuntu',
+    //   host : '',
+    //   ref  : 'origin/master',
+    //   repo : '',
+    //   path : '',
+    //   'pre-deploy-local': '',
+    //   'post-deploy' : 'nvm use 14.18.2 && npm install && npm run build && pm2 restart ecosystem.config.js',
+    //   'pre-setup': ''
+    // }    
+  }
+};
