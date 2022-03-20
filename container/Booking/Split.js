82a279658c0b89fe32b6dcfd3d51967a7b4b5146
Muaaz Rafi <tomuaaz@gmail.com>
2022-03-20 23:10:56 +0500

    Need to complete the split payment implementation

--- container/Booking/Split.js
+++ container/Booking/Split.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import { Card, Row, Col } from 'antd';
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { Card, Row, Col, InputNumber, Button } from "antd";
+import ApartmentCurrency from "container/SinglePage/ApartmentCurrency/ApartmentCurrency";
+import CardDetails from "../Payment/CardDetails";
 
 const Split = (props) => {
+  const { booking, loading } = useSelector(state => state.booking);
+  const [amount, setAmount] = useState(0);
+
   return (
-    <Card title='Make Payment' >
-      
+    <Card title="Make Payment">
+      <CardDetails />
+      <Row style={{ marginTop: '8px' }} >
+        <Col span={24}>
+          <strong>Amount (Pending <ApartmentCurrency currency={booking.currency} />{booking.pending_balance}) </strong>
+          <InputNumber
+            placeholder="Enter amount you want to contribute."
+            onChange={(value) => setAmount(value)}
+            min={.1}
+            precision={2}
+            step={0.1}
+            max={booking.pending_balance}
+            style={{ width: '100%', margin: '2px 0px 8px 0px' }}
+          />
+          <Button type="primary" style={{ width: '100%' }} >
+            Pay Now (<ApartmentCurrency currency={booking.currency} />{amount})
+          </Button>
+        </Col>
+      </Row>
     </Card>
-  )
-}
+  );
+};
 
 export default Split;
--- pages/splits/[id].js
+++ pages/splits/[id].js
@@ -1,42 +1,56 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
-import { Row, Col } from 'antd';
+import { Row, Col } from "antd";
 import VerifyAuth from "container/Auth/VerifyAuth";
 import ResendConfirmation from "/container/Auth/ResendConfirmation";
 import Nav from "/container/Dashboard/Nav";
 import { FormContent } from "/container/Stylis/InnerContainer.style";
 import { fetchActiveBooking } from "store/services/booking";
+import AmountPaid from "container/Booking/AmountPaid";
+import InviteFriends from "/container/Booking/InviteFriends";
+import ShareLink from "/container/Booking/ShareLink";
 import Split from "/container/Booking/Split";
+
+const stripePromise = loadStripe("pk_test_oJYwZZaKNdW0Qzl1asjCwD9B");
 
 const Dashboard = (props) => {
   const router = useRouter();
   const { id } = router.query;
+  const { booking } = useSelector((state) => state.booking);
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (id) {
       dispatch(fetchActiveBooking(id));
     }
-  }, []);
+  }, [id]);
 
   return (
     <>
       <VerifyAuth />
       <ResendConfirmation />
-      <FormContent>
-        <Nav current="bookings" />
-        <h3>Complete Your Payment</h3>
-        <Row gutter={10}>
-          <Col md={12} sm={24} xs={24}>
-            <Split />
-          </Col>
-          <Col md={12} sm={24} xs={24}>
-
-          </Col>
-        </Row>
-
-      </FormContent>
+      <Elements stripe={stripePromise}>
+        <FormContent>
+          <Nav current="bookings" />
+          <h3>Complete Your Payment</h3>
+          <Row gutter={10}>
+            <Col md={12} sm={24} xs={24}>
+              <Split />
+              <br />
+              <InviteFriends booking={booking} />
+              <br />
+              <ShareLink booking={booking} />
+            </Col>
+            <Col md={12} sm={24} xs={24}>
+              {booking && <AmountPaid booking={booking} />}
+              <br />
+            </Col>
+          </Row>
+        </FormContent>
+      </Elements>
     </>
   );
 };
