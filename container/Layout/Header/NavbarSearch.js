671025e9ea6a1d7746b5ca00f13a8456301c42f4
Muaaz Rafi <tomuaaz@gmail.com>
2022-03-23 23:29:47 +0500

    Need to persist search

--- container/Layout/Header/NavbarSearch.js
+++ container/Layout/Header/NavbarSearch.js
@@ -1,11 +1,17 @@
 import React, { useContext } from "react";
-import Router, { withRouter } from "next/router";
+import { useRouter } from "next/router";
 import { Select } from "antd";
 import { NavbarSearchWrapper } from "./Header.style";
 
 const { Option } = Select;
 
 const NavbarSearch = () => {
+  const router = useRouter();
+
+  const changeCity = (value) => {
+
+  }
+
   return (
     <NavbarSearchWrapper className="navbar_search">
       <Select
@@ -13,6 +19,7 @@ const NavbarSearch = () => {
         style={{ paddingLeft: "10px", width: "150px" }}
         placeholder="City"
         optionFilterProp="children"
+        onChange={changeCity}
       >
         <Option value="barcelona">Barcelona</Option>
         <Option value="madrid">Madrid</Option>
@@ -21,4 +28,4 @@ const NavbarSearch = () => {
   );
 };
 
-export default withRouter(NavbarSearch);
+export default NavbarSearch;
