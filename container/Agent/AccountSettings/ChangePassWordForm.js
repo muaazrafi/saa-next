e1100910cd274bea2e690cefdf51dabdad50d3e2
Muaaz Rafi <tomuaaz@gmail.com>
2022-03-29 21:31:49 +0500

    Completed the password update

--- container/Agent/AccountSettings/ChangePassWordForm.js
+++ container/Agent/AccountSettings/ChangePassWordForm.js
@@ -1,83 +1,87 @@
-import React from 'react';
-import { useForm, Controller } from 'react-hook-form';
-import { Input, Button, Row, Col } from 'antd';
-import FormControl from 'components/UI/FormControl/FormControl';
-import { FormTitle } from './AccountSettings.style';
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Form, Input, Button, Row, Col } from "antd";
+import { LockOutlined } from "@ant-design/icons";
+import { handleLoading } from "store/authSlice";
+import { updateMe } from "store/services/auth";
 
 export default function ChangePassWord() {
-  const { control, errors, watch, handleSubmit } = useForm({
-    mode: 'onChange',
-  });
-  const newPassword = watch('newPassword');
-  const confirmPassword = watch('confirmPassword');
-  const onSubmit = (data) => console.log(data);
+  const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.auth);
+
+  const onFinish = (values) => {
+    dispatch(handleLoading(true));
+    dispatch(updateMe(values));
+  };
 
   return (
     <>
       <h2>Change Password</h2>
-      <form className="form-container" onSubmit={handleSubmit(onSubmit)}>
-        <Row gutter={30}>
-          <Col span={24}>
-            <FormControl
-              label="Enter new password"
-              htmlFor="newPassword"
-              error={
-                errors.newPassword && (
-                  <>
-                    {errors.newPassword?.type === 'required' && (
-                      <span>This field is required!</span>
-                    )}
-                    {errors.newPassword?.type === 'minLength' && (
-                      <span>New password must be at lest 6 characters!</span>
-                    )}
-                    {errors.newPassword?.type === 'maxLength' && (
-                      <span>
-                        New password must not be longer than 20 characters!
-                      </span>
-                    )}
-                  </>
-                )
-              }
-            >
-              <Controller
-                as={<Input.Password />}
-                defaultValue=""
-                control={control}
-                id="newPassword"
-                name="newPassword"
-                rules={{ required: true, minLength: 6, maxLength: 20 }}
-              />
-            </FormControl>
-          </Col>
-          <Col span={24}>
-            <FormControl
-              label="Confirm new password"
-              htmlFor="confirmPassword"
-              error={
-                confirmPassword &&
-                newPassword !== confirmPassword && (
-                  <span>Confirm password must be the same!</span>
-                )
-              }
-            >
-              <Controller
-                as={<Input.Password />}
-                defaultValue=""
-                control={control}
-                id="confirmPassword"
-                name="confirmPassword"
-              />
-            </FormControl>
-          </Col>
-          <Col lg={24}>
-            <div className="submit-container">
-              <Button htmlType="submit" type="primary">
-                Save Changes
-              </Button>
-            </div>
-          </Col>
-        </Row>
-      </form>
+      <Form
+        name="password"
+        className="password-form"
+        onFinish={onFinish}
+        layout="vertical"
+      >
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+            { min: 8, message: "Password must be minimum 8 characters." },
+          ]}
+          style={{ marginBottom: "20px" }}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="password_confirmation"
+          label="Password Confirmation"
+          rules={[
+            {
+              required: true,
+              message: "Please input confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  "Password that you entered do not match!"
+                );
+              },
+            }),
+          ]}
+          style={{ marginBottom: "20px" }}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="Password Confirmation"
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button
+            type="primary"
+            size="middle"
+            htmlType="submit"
+            block
+            loading={loading}
+          >
+            Update Password
+          </Button>
+        </Form.Item>
+      </Form>
     </>
   );
 }
