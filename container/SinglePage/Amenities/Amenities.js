52b0aae6f3336f710e72a1e32999fceebed85ff1
Muaaz Rafi <tomuaaz@gmail.com>
2022-04-16 15:00:30 +0500

    Amaenties fixed

--- container/SinglePage/Amenities/Amenities.js
+++ container/SinglePage/Amenities/Amenities.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import Link from 'next/link';
-import PropTypes from 'prop-types';
+import React from "react";
+import Link from "next/link";
+import PropTypes from "prop-types";
 import { useSelector } from "react-redux";
-import Heading from 'components/UI/Heading/Heading';
-import { Row,Col } from 'antd';
-import { FaWifi, FaCarAlt, FaSwimmer, FaAirFreshener,FaCheck } from 'react-icons/fa';
-import IconCard from 'components/IconCard/IconCard';
-import AmenitiesWrapper, { AmenitiesArea,AmenitiesAreaMore } from './Amenities.style';
-import { TextButton } from '../SinglePageView.style';
-import { Element } from 'react-scroll';
+import { capitalize } from "lodash";
+import Heading from "components/UI/Heading/Heading";
+import { Row, Col } from "antd";
+import { FaBath, FaCheck } from "react-icons/fa";
+import { MdMeetingRoom, MdHomeWork } from "react-icons/md";
+import { IoBed } from "react-icons/io5";
+import IconCard from "components/IconCard/IconCard";
+import AmenitiesWrapper, {
+  AmenitiesArea,
+  AmenitiesAreaMore,
+} from "./Amenities.style";
+import { TextButton } from "../SinglePageView.style";
+import { Element } from "react-scroll";
 
 const Amenities = ({ titleStyle, linkStyle }) => {
   const { apartment, loading } = useSelector((state) => state.apartment);
@@ -16,25 +22,43 @@ const Amenities = ({ titleStyle, linkStyle }) => {
     <Element name="amenities" className="Amenities">
       <AmenitiesWrapper>
         <Heading as="h2" content="Amenities" {...titleStyle} />
-        <AmenitiesArea>
-          <IconCard icon={<FaWifi />} title="Free wifi" />
-          <IconCard icon={<FaCarAlt />} title="Free parking" />
-          <IconCard icon={<FaSwimmer />} title="Free pool" />
-          <IconCard icon={<FaAirFreshener />} title="Air Freshener" />
-        </AmenitiesArea>
-        <TextButton style={{marginBottom: '10px'}}>
+        {apartment && (
+          <AmenitiesArea>
+            <IconCard
+              icon={<MdMeetingRoom />}
+              title={`${apartment.number_of_bedrooms} Bedrooms`}
+            />
+            <IconCard
+              icon={<IoBed />}
+              title={`${apartment.number_of_beds} Bedrooms`}
+            />
+            <IconCard
+              icon={<FaBath />}
+              title={`${apartment.number_of_bathrooms} Bathrooms`}
+            />
+            <IconCard
+              icon={<MdHomeWork />}
+              title={capitalize(apartment.apart_type)}
+            />
+          </AmenitiesArea>
+        )}
+
+        <TextButton style={{ marginBottom: "10px" }}>
           <Link href="#1">
             <a style={{ ...linkStyle }}>All Amenities</a>
           </Link>
-        </TextButton>  
+        </TextButton>
         <AmenitiesAreaMore>
-          <Row gutter={[16, 16]} style={{ width: '100%' }} >
-            {apartment && apartment.amenities.map((amenitie) =>
-              <Col lg={6} md={6} sm={8} xs={8}>{<FaCheck/>}{amenitie}</Col>
-            )}
+          <Row gutter={[16, 16]} style={{ width: "100%" }}>
+            {apartment &&
+              apartment.amenities.map((amenitie) => (
+                <Col lg={6} md={6} sm={8} xs={8}>
+                  {<FaCheck />}
+                  {amenitie}
+                </Col>
+              ))}
           </Row>
         </AmenitiesAreaMore>
-
       </AmenitiesWrapper>
     </Element>
   );
@@ -47,15 +71,15 @@ Amenities.propTypes = {
 
 Amenities.defaultProps = {
   titleStyle: {
-    color: '#2C2C2C',
-    fontSize: ['17px', '20px', '25px'],
-    lineHeight: ['1.15', '1.2', '1.36'],
-    mb: ['14px', '20px', '30px'],
+    color: "#2C2C2C",
+    fontSize: ["17px", "20px", "25px"],
+    lineHeight: ["1.15", "1.2", "1.36"],
+    mb: ["14px", "20px", "30px"],
   },
   linkStyle: {
-    fontSize: '15px',
-    fontWeight: '700',
-    color: '#0088E5',
+    fontSize: "15px",
+    fontWeight: "700",
+    color: "#0088E5",
   },
 };
 
