ba2b679a24a3d1d8337cc95c2218154a674b1a49
Muaaz Rafi <tomuaaz@gmail.com>
2022-04-23 02:05:56 +0500

    Preparing production deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,15 +17,15 @@ module.exports = {
       'post-deploy' : 'nvm use 16.13.0 && yarn && yarn build && pm2 restart ecosystem.config.js',
       'pre-setup': ''
     },
-    // production : {
-    //   user : 'ubuntu',
-    //   host : '',
-    //   ref  : 'origin/master',
-    //   repo : '',
-    //   path : '',
-    //   'pre-deploy-local': '',
-    //   'post-deploy' : 'nvm use 14.18.2 && npm install && npm run build && pm2 restart ecosystem.config.js',
-    //   'pre-setup': ''
-    // }    
+    production : {
+      user : 'ubuntu',
+      host : ['34.209.166.229', '54.203.55.174', '34.223.104.22'],
+      ref  : 'origin/master',
+      repo : 'https://muaazrafi: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@github.com/muaazrafi/saa-next.git',
+      path : '/home/ubuntu/saa_next',
+      'pre-deploy-local': '',
+      'post-deploy' : 'nvm use 16.13.0 && yarn && yarn build && pm2 restart ecosystem.config.js',
+      'pre-setup': ''
+    }    
   }
 };
