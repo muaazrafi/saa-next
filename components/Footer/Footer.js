73696e37a1ec44380e6a216c0fe258982b513af7
Muaaz Rafi <tomuaaz@gmail.com>
2022-10-31 12:00:21 +0500

    Adding clairty code in to view how booking form is getting populated with wrong dates

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -153,6 +153,17 @@ const Footer = () => {
             ga('send', 'pageview');
         `}
       </Script>
+      <Script id='clarity_code' strategy='lazyOnload'>
+        {`
+          <script type="text/javascript">
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ebihez4z5r");
+          </script>
+        `}
+      </Script>
     </FooterWrapper>
   );
 };
